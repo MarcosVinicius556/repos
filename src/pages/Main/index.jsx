3f0cae2a3de624116f3aa682a90a495408b43f03
Marcos Vinicius <marcosvinicios4132@gmail.com>
2023-08-29 16:27:15 -0300

    Estilização da tela de busca de repositórios criada

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
-
-import {Title} from './styles';
+import { FaGithub, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main(){
   return(
-    <Title>
-      Main
-    </Title>
+    <Container>
+
+      <h1>
+        <FaGithub size={25} />
+        Meus Repositórios
+      </h1>
+      {/*Normalmente quando se tem mais de 2 
+        encadeamentos de componentes, um novo é criado....  */}
+      <Form onSubmit={()=>{}}>
+        <input type="text" placeholder='Adicionar Repositórios' />
+
+        <SubmitButton>
+          <FaPlus color='#fff' size={14} />
+        </SubmitButton>
+      </Form>
+
+    </Container>
   )
 }
