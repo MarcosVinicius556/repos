6bd9c786fe444f7e7a1d7b7e2b38e5a8d6794b34
Marcos Vinicius <marcosvinicios4132@gmail.com>
2023-08-29 16:11:16 -0300

    Aplicação de styled components na aplicação

---
+++ src/pages/Repositorio/index.jsx
@@ -0,0 +1,9 @@
+function Repositorio() {
+    return(
+        <div>
+            <h1>Main</h1>
+        </div>
+    )
+}
+
+export default Repositorio;
---
+++ src/routes/index.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Main from "../pages/Main";
+import Repositorio from "../pages/Repositorio";
+
+function RoutesApp() {
+    return(
+        <Routes>
+            <Route path='/' element={ <Main /> } />
+            <Route path='/repositorio/:repositorio' element={ <Repositorio /> } />
+        </Routes>
+    );
+}
+
+export default RoutesApp;
