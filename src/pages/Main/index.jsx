510fbd94084c4b70d43a5905164404e851b8900f
Marcos Vinicius <marcosvinicios4132@gmail.com>
2023-08-29 17:02:23 -0300

    useCallback aplicado na página main, requisição com axios criada, busca de repositórios criada

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import { useState, useCallback } from 'react';
 import { FaGithub, FaPlus } from 'react-icons/fa';
 import { Container, Form, SubmitButton } from './styles';
 
+import api from '../../services/api';
+
 export default function Main(){
+
+  const[newRepo, setNewRepo] = useState('');
+  const[repositorios, setRepositorios] = useState([]);
+
+  function handleInputChange(e) {
+    setNewRepo(e.target.value);
+  }
+
+  //Ao algum dos estados mudarm o callback será chamado e executará as funções contidas dentro
+  const handleSubmit = useCallback((e) => {
+    e.preventDefault();
+    async function submit() {
+      const response = await api.get(`repos/${newRepo}`);
+
+      const data = {
+        name: response.data.full_name,
+      }
+
+      //Mantém o que já tinha e adiciona mais
+      setRepositorios([ ...repositorios, data ]);
+      setNewRepo('');
+    }
+
+    submit();
+  }, [newRepo, repositorios])
+
   return(
     <Container>
 
@@ -12,8 +40,8 @@ export default function Main(){
       </h1>
       {/*Normalmente quando se tem mais de 2 
         encadeamentos de componentes, um novo é criado....  */}
-      <Form onSubmit={()=>{}}>
-        <input type="text" placeholder='Adicionar Repositórios' />
+      <Form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Adicionar Repositórios' value={newRepo} onChange={handleInputChange}/>
 
         <SubmitButton>
           <FaPlus color='#fff' size={14} />
