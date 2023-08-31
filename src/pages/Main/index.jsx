6ac9dacc9c2147e46b0fbd60c8026ff209422e64
Marcos Vinicius <marcosvinicios4132@gmail.com>
2023-08-31 14:58:24 -0300

    Remoção de repositório da lista, estilização da lista de repositórios

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,6 +1,6 @@
 import { useState, useCallback } from 'react';
-import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
-import { Container, Form, SubmitButton } from './styles';
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
+import { Container, Form, SubmitButton, List, DeleteButton } from './styles';
 
 import api from '../../services/api';
 
@@ -37,7 +37,23 @@ export default function Main(){
       }  
 
     submit();
-  }, [newRepo, repositorios])
+  }, [newRepo, repositorios]);
+
+  /**
+   * Utilizando useCallback novamente,
+   * pois sempre que se está manipulando 
+   * estados com react é mais interessante 
+   * utilziar o useCallback
+   */
+
+  const handleDelete = useCallback((repo) => {
+    /**
+     * Verifica e retorna todos que sejam diferentes do escolhido
+     */
+    const find = repositorios.filter(r => r.name != repo);
+
+    setRepositorios(find);
+  }, [repositorios]);
 
   return(
     <Container>
@@ -62,6 +78,24 @@ export default function Main(){
           
         </SubmitButton>
       </Form>
+
+      <List>
+          {
+            repositorios.map(repo => (
+              <li key={repo.name}>
+                <span>
+                  <DeleteButton onClick={() => handleDelete(repo.name)}>
+                    <FaTrash size={14} />
+                  </DeleteButton>
+                  {repo.name}
+                </span>
+                <a href="">
+                  <FaBars size={20}/>
+                </a>
+              </li>
+            ))
+          }
+      </List>
 
     </Container>
   )
