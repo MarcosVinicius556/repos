c6365f5ca4ad52e16d9c528852e3476cfec79d60
Marcos Vinicius <marcosvinicios4132@gmail.com>
2023-09-01 09:46:04 -0300

    Salvando repositórios no localStorage

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
 import { Container, Form, SubmitButton, List, DeleteButton } from './styles';
 
@@ -9,19 +9,50 @@ export default function Main(){
   const[newRepo, setNewRepo] = useState('');
   const[repositorios, setRepositorios] = useState([]);
   const[loading, setLoading] = useState(false);
+  const[alert, setAlert] = useState(null);
+
+  //DIdMount (Ao componente ser criado) - Busca
+  useEffect(() => {
+    const repoStorage = localStorage.getItem('@repos');
+    
+    if(repoStorage){
+      setRepositorios(JSON.parse(repoStorage));
+    }
+
+  }, []);
+
+  //DidUpdate (Ao componente ser atualizado) - Salva
+  useEffect(() => {
+    localStorage.setItem('@repos', JSON.stringify(repositorios));
+
+  }, [repositorios]); //Ao alterar esta state, executa esta função
 
   function handleInputChange(e) {
     setNewRepo(e.target.value);
+    setAlert(null);
   }
 
   //Ao algum dos estados mudarm o callback será chamado e executará as funções contidas dentro
   const handleSubmit = useCallback((e) => {
     e.preventDefault();
     setLoading(true);
+    setAlert(null);
     async function submit() {
         try {
+
+            if(newRepo === '') {
+              throw new Error('Você precisa indicar um repositório!');
+            }
+
             const response = await api.get(`repos/${newRepo}`);
     
+            //Passando por todos os repo, e verificando se tem algum com o mesmo nome informado
+            const hasRepo = repositorios.find(repo => repo.name === newRepo);
+
+            if(hasRepo){
+              throw new Error('Repositório duplicado');
+            }
+
             const data = {
               name: response.data.full_name,
             }
@@ -30,6 +61,7 @@ export default function Main(){
             setRepositorios([ ...repositorios, data ]);
             setNewRepo('');
         } catch (error) {
+            setAlert(true);
             console.log(error);
         } finally {
             setLoading(false);
@@ -64,7 +96,7 @@ export default function Main(){
       </h1>
       {/*Normalmente quando se tem mais de 2 
         encadeamentos de componentes, um novo é criado....  */}
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit} error={alert}>
         <input type="text" placeholder='Adicionar Repositórios' value={newRepo} onChange={handleInputChange}/>
 
         <SubmitButton loading={loading ? 1 : 0}>
