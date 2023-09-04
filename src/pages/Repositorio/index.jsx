a3e715a0c6a6f2003dfe57a290e27e58b3764dde
Marcos Vinicius <marcosvinicios4132@gmail.com>
2023-09-04 17:32:16 -0300

    Listando e estilizando as issues

--- src/pages/Repositorio/index.jsx
+++ src/pages/Repositorio/index.jsx
@@ -1,6 +1,6 @@
 import { useParams} from "react-router-dom";
 import { useState, useEffect } from 'react';
-import { Container, Owner, Loading, BackButton } from "./styles";
+import { Container, Owner, Loading, BackButton, IssuesList } from "./styles";
 import { FaArrowLeft } from 'react-icons/fa';
 import api from '../../services/api';
 
@@ -62,6 +62,28 @@ function Repositorio() {
                 <h1>{ repo.name }</h1>
                 <p>{ repo.description}</p>
             </Owner>
+            <IssuesList>
+                {
+                    issues.map((issue) => (
+                        <li key={String(issue.id)}> {/**Convertendo o id da issue para string */}
+                            <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+                            <div>
+                                <strong>
+                                    <a href={issue.html_url}>{issue.title}</a>
+                                    {issue.labels.map(label => (
+                                        <span key={String(label.id)}> {label.name} </span>
+                                    ))}
+                                </strong>
+
+                                <p>{issue.user.login}</p>
+
+                            </div>
+
+                        </li>
+                    ))
+                }
+            </IssuesList>
         </Container>
     )
 }
